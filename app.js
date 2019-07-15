6b6506a9e76eae944a11f24fda05b629a4d4f9c3
Tomek Felisiak <tomekfel@googlemail.com>
2019-07-15 21:57:46 +0100

    js added

---
+++ app.js
@@ -0,0 +1,73 @@
+const app = () => {
+  const song = document.querySelector(".song");
+  const play = document.querySelector(".play");
+  const outline = document.querySelector(".moving-outline circle");
+  const video = document.querySelector(".vid-container video");
+
+  //   Sounds
+  const sounds = document.querySelectorAll(".sound-picker button");
+
+  // Time Display
+  const timeDisplay = document.querySelector(".time-display");
+  const timeSelect = document.querySelectorAll(".time-select button");
+
+  //   Get the length of the outline
+  const outlineLength = outline.getTotalLength();
+  // Duration
+  let fakeDuration = 60;
+
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+
+  //   play sound
+  play.addEventListener("click", function() {
+    checkPlaying(song);
+  });
+
+  // Select sound
+  timeSelect.forEach(option => {
+    option.addEventListener("click", function() {
+      fakeDuration = this.getAttribute("data-time");
+      timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+        fakeDuration % 60
+      )}`;
+    });
+  });
+
+  // Create a function to stop and play the sounds
+  const checkPlaying = song => {
+    if (song.paused) {
+      song.play();
+      play.src = "./svg/pause.svg";
+      video.play();
+    } else {
+      song.pause();
+      play.src = "./svg/play.svg";
+      video.pause();
+    }
+  };
+
+  // Animate the player progress
+  song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
+
+    //   Animate the circle
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+
+    // Animate the text
+    timeDisplay.textContent = `${minutes}:${seconds}`;
+
+    if (currentTime >= fakeDuration) {
+      song.pause();
+      song.currentTime = 0;
+      play.src = "./svg/play.svg";
+      video.pause();
+    }
+  };
+};
+
+app();
